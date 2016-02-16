453af74be512272655659ae4f17bf1f42b96ec4b
kris <kris@cognitivlabs.com>
2016-02-16 02:49:21 -0500

    updated files bnadit_classes2.py, bandits_static2, and bandits_volatile_updating

--- bandit_classes.py
+++ bandit_classes.py
@@ -1,42 +1,42 @@
-import numpy as np
 from math import sqrt
+import numpy as np
+from numpy.random import choice
 
 class AB(object):
-    def __init__(self, n_arms,
-                 default_count=1.0,
-                 default_value=1.0,
-                stop_after=30):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
+    def __init__(self, n_arms, stop_after=30):
         self.n = n_arms
-        self.default_count = default_count
-        self.default_value = default_value
-        self.counter = 0
         self.stop_after = stop_after
+        self.arms = {}
+        for i in range(self.n):
+            self.arms[i] = {}
+            self.arms[i]['observations'] = []
 
     def choose_arm(self):
-        self.counter = (self.counter + 1) % self.n
-        counts = sorted([(self.counts[arm], arm) for arm in range(self.n)])
-        if counts[0][0] < self.stop_after:
-            return counts[0][1]
-        else:# self.counter > self.n*self.stop_after:
-            return np.argmax(self.values)
+        counts = [(len(self.arms[arm_id]['observations']), arm_id) for arm_id in self.arms.keys()]
+        if any([c[0] < self.stop_after for c in counts]):
+            choices = [c[1] for c in counts if c[0] < self.stop_after]
+            return choice(choices)
+        else:
+            means = [self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
+            return np.max(means)[1]
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
-        self.counter = 0
+    def reset_arm(self, arm_id):
+        self.arms[i]['observations'] = []
 
-    def update(self, arm, reward):
-        self.counts[arm] = self.counts[arm] + 1
-        n = self.counts[arm]
-        value = self.values[arm]
-        new_value = ((n - 1) / float(n)) * value
-        new_value += (1 / float(n)) * reward
-        self.values[arm] = new_value
+    def update(self, arm_id, reward):
+        self.arms[arm_id]['observations'].append(reward)
 
-    def get_arm_data(self):
-        return list(zip(self.values, np.zeros(self.n)))
+    def get_expected_value(self, arm_id):
+        return np.mean(self.arms[arm_id]['observations'])
+
+    def get_variance(self, arm_id):
+        return np.var(self.arms[arm_id]['observations'])
+
+    def get_expected_values_all(self):
+        return list(map(lambda x: self.get_arm_data_expected_value(x), self.arms.keys()))
+
+    def get_variances_all(self):
+        return list(map(lambda x: self.get_variance(x), self.arms.keys()))
 
 class Greedy(object):
     def __init__(self, n_arms,
@@ -224,14 +224,14 @@ class Bayesian2(object):
             self.arms[i]['a'] = self.default_alpha
             self.arms[i]['b'] = self.default_beta
 
-    def choose_arm(self):
-        return choose_arms(k_arms=1)[0]
-
     def choose_arms(self, k_arms=1):
         res = []
         for i in self.arms.keys():
             res.append((np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i))
         return [e[1] for e in sorted(res, reverse=True)[:k_arms]]
+
+    def choose_arm(self):
+        return self.choose_arms(k_arms=1)[0]
 
     def add_arm(self):
         self.n += 1
@@ -287,15 +287,14 @@ class Bayesian3(object):
             self.arms[i]['a'] = self.default_alpha
             self.arms[i]['b'] = self.default_beta
 
-    def choose_arm(self):
-        return choose_arms(k_arms=1)[0]
-
-
     def choose_arms(self, k_arms=1):
         res = []
         for i in self.arms.keys():
             res.append((np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i))
         return [e[1] for e in sorted(res, reverse=True)[:k_arms]]
+
+    def choose_arm(self):
+        return self.choose_arms(k_arms=1)[0]
 
     def add_arm(self):
         self.n += 1
@@ -331,14 +330,23 @@ class Bayesian3(object):
                     self.arms[arm_id]['a'] = (self.arms[arm_id]['a']+1)*C/float(C+1)
                     self.arms[arm_id]['b'] *= C/float(C+1)
 
-    def get_arm_data(self, arm_id):
+    def get_expected_value(self, arm_id):
         a = self.arms[arm_id]['a']
         b = self.arms[arm_id]['b']
         expected_value = a/(a+b)
-        return (expected_value, 0.0, arm_id)
+        return expected_value
 
-    def get_arm_data_all(self):
-        return list(map(lambda x: self.get_arm_data(x), self.arms.keys()))
+    def get_variance(self, arm_id):
+        a = self.arms[arm_id]['a']
+        b = self.arms[arm_id]['b']
+        variance = a*b/(((a+b)**2)*(a+b+1))
+        return varience
+
+    def get_expected_values_all(self):
+        return list(map(lambda x: self.get_arm_data_expected_value(x), self.arms.keys()))
+
+    def get_variances_all(self):
+        return list(map(lambda x: self.get_variance(x), self.arms.keys()))
 
 if __name__ == '__main__':
     b = Bayesian3()
--- bandit_data.py
+++ bandit_data.py
@@ -2,21 +2,16 @@
 
 import numpy as np
 
-class BanditData(object):
-	def __init__(self):
-		pass
+def gen_static_uniform(k = 2, T = 10):
+	temp = np.random.rand(k)
+	rewards = np.zeros((T, k))
+	for t in range(T):
+		rewards[t] += temp
+	return rewards
 
-	def gen_static_uniform(self, k = 2, T = 10):
-		temp = np.random.rand(k)
-		rewards = np.zeros((T, k))
-		for t in range(T):
-			rewards[t] += temp
-		return rewards
-
-	def gen_static_beta(self, k = 2, T = 10, a = 1, b = 10):
-		temp = np.random.beta(a, b, k)
-		rewards = np.zeros((T, k))
-		for t in range(T):
-			rewards[t] += temp
-		return rewards
-		
+def gen_static_beta(k = 2, T = 10, a = 1, b = 10):
+	temp = np.random.beta(a, b, k)
+	rewards = np.zeros((T, k))
+	for t in range(T):
+		rewards[t] += temp
+	return rewards
