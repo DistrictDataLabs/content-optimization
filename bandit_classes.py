d247b99fc394b564588148fbb21b7280dcc671a1
kris <kris@cognitivlabs.com>
2016-02-27 00:44:10 -0500

    removed/renamed some files

--- bandit_classes.py
+++ bandit_classes.py
@@ -2,362 +2,234 @@ from math import sqrt
 import numpy as np
 from numpy.random import choice
 
-class AB(object):
-    def __init__(self, n_arms, stop_after=30):
-        self.n = n_arms
-        self.stop_after = stop_after
-        self.arms = {}
-        for i in range(self.n):
-            self.arms[i] = {}
-            self.arms[i]['observations'] = []
+class Bandit(object):
+	def __init__(self, n_arms):
+		self.n = n_arms
+		self.arms = {}
+		for i in range(self.n):
+			self.arms[i] = {}
+			self.arms[i]['observations'] = [1,0]
 
-    def choose_arm(self):
-        counts = [(len(self.arms[arm_id]['observations']), arm_id) for arm_id in self.arms.keys()]
-        if any([c[0] < self.stop_after for c in counts]):
-            choices = [c[1] for c in counts if c[0] < self.stop_after]
-            return choice(choices)
-        else:
-            means = [self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
-            return np.max(means)[1]
+	def add_arm(self, arm_id = None):
+		if not arm_id:
+			self.n += 1
+			arm_id = self.n
+		else:
+			arm_id = arm_id
+		self.arms[arm_id] = {}
+		self.arms[arm_id]['observations'] = [1,0]
+		return arm_id
 
-    def reset_arm(self, arm_id):
-        self.arms[i]['observations'] = []
+	def remove_arm(self, arm_id):
+		assert arm_id in self.arms.keys()
+		del self.arms[arm_id]
 
-    def update(self, arm_id, reward):
-        self.arms[arm_id]['observations'].append(reward)
+	def reset_arm(self, arm_id):
+		self.arms[i]['observations'] = [1,0]
 
-    def get_expected_value(self, arm_id):
-        return np.mean(self.arms[arm_id]['observations'])
+	def update(self, arm_id, reward):
+		self.arms[arm_id]['observations'].append(reward)
 
-    def get_variance(self, arm_id):
-        return np.var(self.arms[arm_id]['observations'])
+	def get_expected_value(self, arm_id):
+		return np.mean(self.arms[arm_id]['observations'])
 
-    def get_expected_values_all(self):
-        return list(map(lambda x: self.get_arm_data_expected_value(x), self.arms.keys()))
+	def get_variance(self, arm_id):
+		n = len(self.arms[arm_id]['observations'])
+		p = sum(self.arms[arm_id]['observations'])/float(n)
+		return p*(1-p)
+		#return np.sqrt(p*(1.0-p)/n)
+		return np.var(self.arms[arm_id]['observations'])
 
-    def get_variances_all(self):
-        return list(map(lambda x: self.get_variance(x), self.arms.keys()))
+	def get_expected_values_all(self):
+		return [self.get_expected_value(x) for x in  self.arms.keys()]
 
-class Greedy(object):
-    def __init__(self, n_arms,
-                 default_count=1.0,
-                 default_value=1.0):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
-        self.n = n_arms
-        self.default_count = default_count
-        self.default_value = default_value
+	def get_variances_all(self):
+		return [self.get_variance(x) for x in self.arms.keys()]
 
-    def choose_arm(self):
-        return np.argmax(self.values)
+class AB(Bandit):
+	def __init__(self, n_arms, stop_after=30):
+		super().__init__(n_arms)
+		self.stop_after = stop_after
+		
+	def choose_arm(self):
+		counts = [(len(self.arms[arm_id]['observations']), arm_id) for arm_id in self.arms.keys()]
+		if any([c[0] < self.stop_after for c in counts]):
+			choices = [c[1] for c in counts if c[0] < self.stop_after]
+			return np.random.choice(choices)
+		else:
+			means = [(self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
+			return sorted(means)[-1][1]
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
+class Greedy(Bandit):    
+	def __init__(self, n_arms):
+		super().__init__(n_arms)
 
-    def update(self, arm, reward):
-        self.counts[arm] = self.counts[arm] + 1
-        n = self.counts[arm]
-        value = self.values[arm]
-        new_value = ((n - 1) / float(n)) * value
-        new_value += (1 / float(n)) * reward
-        self.values[arm] = new_value
+	def choose_arm(self):
+		means = [(self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
+		return sorted(means)[-1][1]
 
-    def get_arm_data(self):
-        return zip(self.values, np.zeros(self.n))
+class EpsilonGreedy(Bandit):
+	def __init__(self, n_arms, epsilon=0.1):
+		super().__init__(n_arms)
+		self.epsilon = epsilon
 
-class EpsilonGreedy(object):
-    def __init__(self, n_arms, epsilon=0.1,
-                 default_count=1.0,
-                 default_value=1.0):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
-        self.epsilon = epsilon
-        self.n = n_arms
-        self.default_count = default_count
-        self.default_value = default_value
+	def choose_arm(self):
+		if np.random.random() > self.epsilon:
+			means = [(self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
+			return sorted(means)[-1][1]
+		else:
+			return np.random.choice(list(self.arms.keys()))
 
-    def choose_arm(self):
-        if np.random.random() > self.epsilon:
-            return np.argmax(self.values)
-        else:
-            return np.random.randint(self.n)
+class EpsilonDecreasing(EpsilonGreedy):
+	def __init__(self, n_arms, epsilon = 0.1, ep_factor = 0.99):
+		super().__init__(n_arms, epsilon)
+		assert (ep_factor < 1.0 and ep_factor > 0.0)
+		self.ep_factor = ep_factor
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
+	def choose_arm(self):
+		self.epsilon *= self.ep_factor
+		if np.random.random() > self.epsilon:
+			means = [(self.get_expected_value(arm_id), arm_id) for arm_id in self.arms.keys()]
+			return sorted(means)[-1][1]
+		else:
+			return np.random.choice(list(self.arms.keys()))
+		
+class UCB(Bandit):
+	def __init__(self, n_arms = 2, delta = 0.0015):
+		super().__init__(n_arms)
+		self.delta = delta
 
-    def update(self, arm, reward):
-        self.counts[arm] = self.counts[arm] + 1
-        n = self.counts[arm]
-        value = self.values[arm]
-        new_value = ((n - 1) / float(n)) * value
-        new_value += (1 / float(n)) * reward
-        self.values[arm] = new_value
+	def get_variance(self, arm_id):
+		T = float(sum([len(self.arms[i]['observations']) for i in self.arms.keys()]))
+		n = float(len(self.arms[arm_id]['observations']))
+		return self.delta*1.96*sqrt(2*np.log(T)/n)
 
-    def get_arm_data(self):
-        return zip(self.values, np.zeros(self.n))
+	def choose_arm(self):
+		ucbs = []
+		for arm_id in self.arms.keys():
+			mean = self.get_expected_value(arm_id)
+			variance = self.get_variance(arm_id)
+			ucbs.append((mean+variance, arm_id))
+		return sorted(ucbs)[-1][1]
 
-class EpsilonDecreasing(object):
-    def __init__(self, n_arms,
-                 default_count=1.0,
-                 default_value=1.0):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
-        self.n = n_arms
-        self.epsilon = (2*self.n)
-        self.epsilon /= (self.counts.sum() + 2*self.n)
-        self.default_count = default_count
-        self.default_value = default_value
+class Bayesian(Bandit):
+	def __init__(self, n_arms = 2, a = 1, b = 1):
+		self.n = n_arms
+		self.arms = {}
+		for i in range(self.n):
+			self.arms[i] = {}
+			self.arms[i]['a'] = a
+			self.arms[i]['b'] = b
+		self.default_alpha = a
+		self.default_beta = b
 
-    def choose_arm(self):
-        self.epsilon = (2*self.n)
-        self.epsilon /= (self.counts.sum() + 2*self.n)
-        if np.random.random() > self.epsilon:
-            return np.argmax(self.values)
-        else:
-            return np.random.randint(self.n)
+	def choose_arm(self):
+		#rvs = []
+		#for arm_id in self.arms.keys():
+		#    rvs.append((np.random.beta(self.arms[arm_id]['a'], self.arms[arm_id]['b']), arm_id))
+		rvs = [(np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i) for i in self.arms.keys()]
+		return sorted(rvs)[-1][1]
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
+	def update(self, arm_id, reward):
+		if reward == 0:
+			self.arms[arm_id]['b'] += 1.0
+		elif reward == 1:
+			self.arms[arm_id]['a'] += 1.0
 
-    def update(self, arm, reward):
-        self.counts[arm] = self.counts[arm] + 1
-        n = self.counts[arm]
-        value = self.values[arm]
-        new_value = ((n - 1) / float(n)) * value
-        new_value += (1 / float(n)) * reward
-        self.values[arm] = new_value
+	def add_arm(self, arm_id = None):
+		if not arm_id:
+			self.n += 1
+			arm_id = self.n
+		else:
+			arm_id = arm_id
+		self.arms[arm_id] = {}
+		self.arms[arm_id]['a'] = self.default_alpha
+		self.arms[arm_id]['b'] = self.default_beta
+		return arm_id
 
-    def get_arm_data(self):
-        return zip(self.values, np.zeros(self.n))
+	def reset_arm(self, arm_id):
+		self.arms[arm_id]['a'] = self.default_alpha
+		self.arms[arm_id]['b'] = self.default_beta
 
-class UCB(object):
-    def __init__(self, n_arms=2,
-                 default_count=1.0,
-                 default_value=1.0):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
-        self.n = n_arms
-        self.default_count = default_count
-        self.default_value = default_value
-        self.delta = 0.015
+	def get_expected_value(self, arm_id):
+		a = self.arms[arm_id]['a']
+		b = self.arms[arm_id]['b']
+		expected_value = a/(a+b)
+		return expected_value
 
-    def choose_arm(self):
-        ucbs = [0.]*self.n
-        T = self.counts.sum()
-        for i in range(self.n):
-            p = self.values[i]
-            n = self.counts[i]
-            ucb = p + self.delta*sqrt(2*T/n)
-            ucbs[i] = ucb
-        return np.argmax(ucbs)
+	def get_variance(self, arm_id):
+		a = self.arms[arm_id]['a']
+		b = self.arms[arm_id]['b']
+		variance = (a*b)/(((a+b)**2)*(a+b+1))
+		return variance
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
+class Bayesian2(Bayesian):
+	def __init__(self, n_arms = 2, a = 1, b = 1, C = 50):
+		super().__init__(n_arms, a, b)
+		self.C = C
+		
+	def choose_arm(self):
+		rvs = [(np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i) for i in self.arms.keys()]
+		return sorted(rvs)[-1][1]
 
-    def update(self, arm, reward):
-        self.counts[arm] = self.counts[arm] + 1
-        n = self.counts[arm]
-        value = self.values[arm]
-        new_value = ((n - 1) / float(n)) * value
-        new_value += (1 / float(n)) * reward
-        self.values[arm] = new_value
+	def update(self, arm_id, reward):
+		C = self.C
+		if reward == 0:
+			if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] > C:
+				self.arms[arm_id]['b'] += 1.0
+			else:
+				self.arms[arm_id]['a'] *= C/float(C+1)
+				self.arms[arm_id]['b'] = (self.arms[arm_id]['b']+1)*C/float(C+1)
 
-    def get_arm_data(self):
-        ci = np.zeros(self.n)
-        T = self.counts.sum()
-        for i in range(self.n):
-            p = self.values[i]
-            n = self.counts[i]
-            ci[i] = self.delta*sqrt(2*T/float(n))
-        return zip(self.values, ci)
+		elif reward == 1:
+			if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] <= C:
+				self.arms[arm_id]['a'] += 1.0
+			else:
+				self.arms[arm_id]['a'] = (self.arms[arm_id]['a']+1)*C/float(C+1)
+				self.arms[arm_id]['b'] *= C/float(C+1)
 
-class Bayesian(object):
-    def __init__(self, n_arms=2,
-                 default_count=1.0,
-                 default_value=1.0):
-        self.counts = np.zeros(n_arms)+default_count
-        self.values = np.zeros(n_arms)+default_value
-        self.n = n_arms
-        self.default_count = default_count
-        self.default_value = default_value
-        self.delta = 0.015
+	def add_arm(self, arm_id = None):
+		if not arm_id:
+			self.n += 1
+			arm_id = self.n
+		else:
+			arm_id = arm_id
+		self.arms[arm_id] = {}
+		self.arms[arm_id]['a'] = self.default_alpha
+		self.arms[arm_id]['b'] = self.default_beta
+		return arm_id
 
-    def choose_arm(self):
-        rvs = np.zeros(self.n)
-        for i in range(self.n):
-            rvs[i] = np.random.beta(self.default_value+self.values[i],
-                               self.default_count+self.counts[i]-self.values[i])
-        return np.argmax(rvs)
+	def reset_arm(self, arm_id):
+		self.arms[arm_id]['a'] = self.default_alpha
+		self.arms[arm_id]['b'] = self.default_beta
 
-    def reset_arm(self, arm):
-        self.counts[arm] = self.default_count
-        self.values[arm] = self.default_value
+	def get_expected_value(self, arm_id):
+		a = self.arms[arm_id]['a']
+		b = self.arms[arm_id]['b']
+		expected_value = a/(a+b)
+		return expected_value
 
-    def update(self, arm, reward):
-        self.counts[arm] += 1
-        self.values[arm] += reward
-
-    def get_arm_data(self):
-        expected_values = np.zeros(self.n)
-        for i in range(self.n):
-            a = self.default_value + self.values[i]
-            b = self.default_count + self.counts[i] - self.values[i]
-            expected_values[i] = a/(a+b)
-        return zip(expected_values, np.zeros(self.n))
-
-class Bayesian2(object):
-    def __init__(self, k=2,
-                 default_alpha=1.0,
-                 default_beta=1.0,
-                 C=20):
-        self.k = k
-        self.C = C
-        self.default_alpha = default_alpha
-        self.default_beta = default_beta
-        self.arms = {}
-        for i in range(self.k):
-            self.arms[i] = {}
-            self.arms[i]['a'] = self.default_alpha
-            self.arms[i]['b'] = self.default_beta
-
-    def choose_arms(self, k_arms=1):
-        res = []
-        for i in self.arms.keys():
-            res.append((np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i))
-        return [e[1] for e in sorted(res, reverse=True)[:k_arms]]
-
-    def choose_arm(self):
-        return self.choose_arms(k_arms=1)[0]
-
-    def add_arm(self):
-        self.n += 1
-        arm_id = self.k
-        self.arms[arm_id] = {}
-        self.arms[arm_id]['a'] = self.default_alpha
-        self.arms[arm_id]['b'] = self.default_beta
-        return arm_id
-
-    def reset_arm(self, arm_id):
-        self.arms[arm_id]['a'] = self.default_alpha
-        self.arms[arm_id]['b'] = self.default_beta
-
-    def remove_arm(self, arm_id):
-        del self.arms[arm_id]
-
-    def update(self, arm_id, reward):
-        C = self.C
-        if reward == 0:
-            if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] > C:
-                self.arms[arm_id]['b'] += 1.0
-            else:
-                self.arms[arm_id]['a'] *= C/float(C+1)
-                self.arms[arm_id]['b'] = (self.arms[arm_id]['b']+1)*C/float(C+1)
-
-        elif reward == 1:
-            if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] <= C:
-                self.arms[arm_id]['a'] += 1.0
-            else:
-                self.arms[arm_id]['a'] = (self.arms[arm_id]['a']+1)*C/float(C+1)
-                self.arms[arm_id]['b'] *= C/float(C+1)
-
-    def get_arm_data(self):
-        expected_values = np.zeros(self.k)
-        for i in range(self.k):
-            a = self.arms[i]['a']
-            b = self.arms[i]['b']
-            expected_values[i] = a/(a+b)
-        return list(zip(expected_values, np.zeros(self.k)))
-
-class Bayesian3(object):
-    def __init__(self, n_arms=2,
-                 default_alpha=1.0,
-                 default_beta=1.0,
-                 C=20):
-        self.n = n_arms
-        self.C = C
-        self.default_alpha = default_alpha
-        self.default_beta = default_beta
-        self.arms = {}
-        for i in range(self.n):
-            self.arms[i] = {}
-            self.arms[i]['a'] = self.default_alpha
-            self.arms[i]['b'] = self.default_beta
-
-    def choose_arms(self, k_arms=1):
-        res = []
-        for i in self.arms.keys():
-            res.append((np.random.beta(self.arms[i]['a'], self.arms[i]['b']), i))
-        return [e[1] for e in sorted(res, reverse=True)[:k_arms]]
-
-    def choose_arm(self):
-        return self.choose_arms(k_arms=1)[0]
-
-    def add_arm(self):
-        self.n += 1
-        arm_id = self.n
-        self.arms[arm_id] = {}
-        self.arms[arm_id]['a'] = self.default_alpha
-        self.arms[arm_id]['b'] = self.default_beta
-        return arm_id
-
-    def reset_arm(self, arm_id):
-        self.arms[arm_id]['a'] = self.default_alpha
-        self.arms[arm_id]['b'] = self.default_beta
-
-    def remove_arm(self, arm_id):
-        del self.arms[arm_id]
-
-    def update(self, arm_id, reward):
-        if arm_id not in self.arms.keys():
-            raise Exception('no such arm currently exists.')
-        else:
-            C = self.C
-            if reward == 0:
-                if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] > C:
-                    self.arms[arm_id]['b'] += 1.0
-                else:
-                    self.arms[arm_id]['a'] *= C/float(C+1)
-                    self.arms[arm_id]['b'] = (self.arms[arm_id]['b']+1)*C/float(C+1)
-
-            elif reward == 1:
-                if self.arms[arm_id]['a'] + self.arms[arm_id]['b'] <= C:
-                    self.arms[arm_id]['a'] += 1.0
-                else:
-                    self.arms[arm_id]['a'] = (self.arms[arm_id]['a']+1)*C/float(C+1)
-                    self.arms[arm_id]['b'] *= C/float(C+1)
-
-    def get_expected_value(self, arm_id):
-        a = self.arms[arm_id]['a']
-        b = self.arms[arm_id]['b']
-        expected_value = a/(a+b)
-        return expected_value
-
-    def get_variance(self, arm_id):
-        a = self.arms[arm_id]['a']
-        b = self.arms[arm_id]['b']
-        variance = a*b/(((a+b)**2)*(a+b+1))
-        return varience
-
-    def get_expected_values_all(self):
-        return list(map(lambda x: self.get_arm_data_expected_value(x), self.arms.keys()))
-
-    def get_variances_all(self):
-        return list(map(lambda x: self.get_variance(x), self.arms.keys()))
+	def get_variance(self, arm_id):
+		a = self.arms[arm_id]['a']
+		b = self.arms[arm_id]['b']
+		variance = (a*b)/(((a+b)**2)*(a+b+1))
+		return variance
 
 if __name__ == '__main__':
-    b = Bayesian3()
-    print(b.get_arm_data_all())
-    b.update(0,1)
-    print(b.get_arm_data_all())
-    b.update(0,1)
-    print(b.get_arm_data_all())
-    b.update(1,1)
-    print(b.get_arm_data_all())
-    b.update(0,1)
-    print(b.get_arm_data_all())
-    print(b.arms[0])
-    print(b.arms[1])
+	b = Bayesian2()
+	print(b.get_expected_values_all())
+	print(b.get_variances_all())
+	b.update(0,1)
+	print(b.get_expected_values_all())
+	print(b.get_variances_all())
+	b.update(0,1)
+	print(b.get_expected_values_all())
+	print(b.get_variances_all())
+	b.update(1,1)
+	print(b.get_expected_values_all())
+	print(b.get_variances_all())
+	b.update(0,1)
+	print(b.get_expected_values_all())
+	print(b.get_variances_all())
+	print(b.arms[0])
+	print(b.arms[1])
